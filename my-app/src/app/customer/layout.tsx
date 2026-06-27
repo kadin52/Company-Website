c5e16f29df9b09d08d8aad60c1f887bf85070d20
kadin52 <ksiphengphone52@gmail.com>
2026-06-27 03:11:21 -0700

    Updated config files

--- my-app/src/app/customer/layout.tsx
+++ my-app/src/app/customer/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/bar/Navbar";
 import LiveChat from "@/components/LiveChat";
 import Footer from "@/components/Footer";
 
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/bar/Navbar";
 import Footer from "../components/Footer";
 import LiveChat from "../components/LiveChat";
 import "./globals.css";
