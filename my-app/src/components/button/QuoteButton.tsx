a711832b1629232d174af137ab035404c5cca896
kadin52 <ksiphengphone52@gmail.com>
2026-07-01 14:45:00 -0700

    Create QuoteButton.tsx

---
+++ my-app/src/components/button/QuoteButton.tsx
@@ -0,0 +1,10 @@
+export default function QuoteButton() {
+  return (
+    <a
+      href="/customer/contact"
+      className="text-white bg-orange-600 hover:bg-orange-300 text-lg font-light p-4 text-center shadow-xl-20"
+    >
+      Request a Quote
+    </a>
+  );
+}
