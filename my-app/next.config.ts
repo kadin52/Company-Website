5aa3731f1a4ea356a49f25ac73eaa7c199ce54a6
kadin52 <ksiphengphone52@gmail.com>
2026-08-03 05:35:44 -0700

    Update next.config.ts

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -5,6 +5,31 @@ const nextConfig: NextConfig = {
    images: {
     qualities: [25, 50, 75,100],
   },
+   async redirects() {
+    return [
+      {
+        source: "/contact.html",
+        destination: "/customer/contact",
+        permanent: true,
+      },
+      {
+        source: "/services.html",
+        destination: "/customer/services",
+        permanent: true,
+      },
+      {
+        source: "/company.html",
+        destination: "/customer/company",
+        permanent: true,
+      },
+      {
+        source: "/index.html",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+  
   async rewrites() {
     return [
       {
