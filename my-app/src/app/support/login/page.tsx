8f0523da73dc1b736d9af038495a4ac658e86aaf
kadin52 <ksiphengphone52@gmail.com>
2026-08-14 15:14:04 -0700

    Update page.tsx

--- my-app/src/app/support/login/page.tsx
+++ my-app/src/app/support/login/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { auth } from "../../../lib/firebase";
+import { auth } from "@/lib/firebase";
 
 import {
   onAuthStateChanged,
